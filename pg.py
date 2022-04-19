49fe9473fc951fe8ce7d217030cd812e31b5f0ab
4vent <abeckento@gmail.com>
2022-04-19 02:06:32 +0000

    edit from pythonista

--- pg.py
+++ pg.py
@@ -1,5 +1,1 @@
-# -*- coding: utf-8 -*-
-
-import console
-delType = console.alert('画像を削除',button1='Delete from Album', button2='Delete from Libraly')
-print(str(delType))
+i
--- test.py
+++
@@ -1,9 +0,0 @@
-class a():
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
-        
-hoge = a('aaa', 'bbb', 'ccc')
-
-print(hoge.a)
